f71d25e3805b4b7ec351baa261eceef7621987a6
anxueying <81247054@qq.com>
2020-04-27 22:56:30 +0800

    项目:记账软件

---
+++ project/src/com/atguigu/java/FamilyAccount.java
@@ -0,0 +1,50 @@
+package com.atguigu.java;
+
+/**
+ * @author Shelly
+ * @version v1.0
+ */
+//import java.util.Scanner;
+public class FamilyAccount {
+    public static void main(String[] args){
+        //Scanner scan = new Scanner(System.in);
+        int balance = 10000;
+        String details = "收支\t账户金额\t收支金额\t说明\n";
+        String markdown = "";
+
+        label:while (true){
+            System.out.print("1.收支明细\n2.登记收入\n3.登记支出\n4.退出\n请选择(1-4)：");
+            char choice = Utility.readMenuSelection();
+            switch (choice){
+                case '1':
+                    System.out.println("------当前收支明细记录------");
+                    //显示所有的收入、支出名细列表
+                    System.out.print(details);
+                    break;
+                case '2':
+                    System.out.print("本次收入金额：");
+                    int earn = Utility.readNumber();
+                    System.out.print("本次收入说明：");
+                    markdown = Utility.readString();
+                    balance += earn;
+                    details += "收入\t"+balance+"\t"+earn+"\t"+markdown+"\n";
+                    break;
+                case '3':
+                    System.out.print("本次支出金额：");
+                    int cost = Utility.readNumber();
+                    System.out.print("本次支出说明：");
+                    markdown = Utility.readString();
+                    balance -= cost;
+                    details += "支出\t"+balance+"\t"+cost+"\t"+markdown+"\n";
+                    break;
+                case '4':
+                    System.out.print("确认是否退出(Y/N)：");//待开发
+                    char ifExit = Utility.readConfirmSelection();
+                    if(ifExit=='Y'){
+                        break label;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ project/src/com/atguigu/java/TestDay4.java
@@ -0,0 +1,54 @@
+package com.atguigu.java;
+
+public class TestDay4 {
+    public static void main(String[] args){
+        int a1=9;
+        while (true){
+            if(a1++>9)
+                break;
+        }
+        System.out.println(a1); //11 在while外，经历两次++
+
+        int a2=9;
+        while (true){
+            if(a2++>9)
+                break;
+            System.out.println(a2); //10 在while内，经历一次++
+        }
+
+
+        int i=10;
+        do{
+            i/=2;
+        }while (i>1);
+        System.out.println(i);
+
+        int b=0;
+        while (b<5){
+            switch (b){
+                case 0:
+                case 3:
+                    b = b+2;
+                case 1:
+                case 2:
+                    b=b+3;
+                default:
+                    b=b+5;
+            }
+        }
+        System.out.println(b);//10 没有break 运行到底
+
+        int j =100;
+        while (true){
+            if(j++>100){
+                break;
+            }
+            System.out.println(j);//101 第二个while被break掉，无法打印
+        }
+
+        for(int k=0;;){
+            System.out.println("这是"+k);
+            break;
+        }
+    }
+}
